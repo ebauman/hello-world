0ae2b333ce15798f368708b7ca428de6ec172e9e
Eamon Bauman <eamon@eamonbauman.com>
2023-07-04 18:33:29 -0500

    more work

--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
-	"fmt"
+	htmlTemplate "html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprint(writer, "Hello, world! (from a VM)")
+		var t = htmlTemplate.Must(htmlTemplate.ParseFiles("./html.tmpl"))
+		var strings = []string{}
+		strings = append(strings, os.Environ()...)
+
+		var out = struct {
+			Output []string
+		}{
+			Output: strings,
+		}
+
+		t.Execute(writer, out)
 	})
 
-	log.Fatal(http.ListenAndServe(":80", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
